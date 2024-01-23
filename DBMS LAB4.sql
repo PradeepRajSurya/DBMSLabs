f374c7d5e3551bb1c861acace852c152d780a7a2
PradeepRajSurya <153101934+PradeepRajSurya@users.noreply.github.com>
2024-01-23 12:30:29 +0530

    Update DBMS LAB4.sql

--- DBMS LAB4.sql
+++ DBMS LAB4.sql
@@ -1,3 +1,10 @@
+4.Consider the schema for College Database:
+  STUDENT(USN,SName,Address,Phone,Gender)
+  SEMSEC(SSID,Sem,Sec)
+  CLASS(USN,SSID)
+  COURSE(Subcode,Title,Sem,Credits)
+  IAMARKS(USN,Subcode,SSID,Test1,Test2,Test3,FinallA)
+
 CREATE TABLE STUDENT (
 USN VARCHAR (15) PRIMARY KEY,
 SNAME VARCHAR (25),
